0310f02172f34ea6745c077f0e1c85b77ab31440
Maham Tahir <106860878+MahamTahir31@users.noreply.github.com>
2023-09-06 21:56:34 +0500

    Add files via upload

---
+++ Project-20/assets/script.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('.ripple');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.pagex;
+        const y = e.pagey;
+
+        const buttonTop = e.target.offsetTop;
+        const buttonLeft = e.target.offsetLeft;
+
+        const xInside = x - buttonLeft;
+        const yInside = y - buttonTop;
+
+        const circle = document.createElement('span');
+
+        circle.classList.add('circle');
+        circle.style.top = yInside + 'px';
+        circle.style.left = xInside + 'px';
+
+        this.appendChild(circle);
+
+        setTimeout(() => circle.remove(), 500);
+    })
+})
